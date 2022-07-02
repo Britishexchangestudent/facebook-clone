b6ecf50d66cdfa05851db2ed60e9de064a4109a4
Dan Ahmed <44755600+Britishexchangestudent@users.noreply.github.com>
2022-07-02 13:17:41 +0100

    + left side home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Header from "../../components/Header";
+import LeftHome from "../../components/Home/Left";
 
 function Home() {
+  const { user } = useSelector((user) => ({ ...user}))
   return (
     <div>
       <Header />
+      <LeftHome user={user} />
     </div>
   );
 }
