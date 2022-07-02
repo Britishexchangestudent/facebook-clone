96fc2bfeb369c7eb9aaed1b045d5c47d9b3f0c96
Dan Ahmed <44755600+Britishexchangestudent@users.noreply.github.com>
2022-07-02 13:49:13 +0100

    + right side home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/Header";
 import LeftHome from "../../components/Home/Left";
+import RightHome from "../../components/Home/Right";
 
 function Home() {
   const { user } = useSelector((user) => ({ ...user}))
@@ -9,6 +10,7 @@ function Home() {
     <div>
       <Header />
       <LeftHome user={user} />
+      <RightHome  />
     </div>
   );
 }
