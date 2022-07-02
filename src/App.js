96e217647026c83618ba26de976b2d439f06d466
Dan Ahmed <44755600+Britishexchangestudent@users.noreply.github.com>
2022-07-02 11:01:43 +0100

    + protected routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes, Route } from "react-router-dom";
+import LoggedInRoutes from "./routes/LoggedInRoutes";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login />} exact />
-        <Route path="/profile" element={<Profile />} exact />
-        <Route path="/" element={<Home />} exact />
+        <Route element={<LoggedInRoutes />}>
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+        </Route>
+        <Route element={<NotLoggedInRoutes />}>
+          <Route path="/login" element={<Login />} exact />
+        </Route>
       </Routes>
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,7 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import Header from "../../components/Header";
-import useClickOutside from "../../helpers/clickOutside";
 
 function Home() {
-  const [visible, setVisible] = useState(true);
-  const el = useRef(null);
-  useClickOutside(el, () => {
-    setVisible(false);
-  });
   return (
     <div>
       <Header />
